96b0b4e8e78b382b9edc3f9003ef3a2945d2c2be
Luis Ayestaran <luisferayestaran98@gmail.com>
2022-01-28 11:26:47 -0600

    Se crea el servicio de autenticacion en Amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "flutteramplifyenviro27ce1792": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    }
+}
